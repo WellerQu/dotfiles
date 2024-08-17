89ec45489ffa5dd82a864eca26fac5b82aee2402
nix <nix@mecx.com>
2024-08-17 12:43:33 +0800

    feat: 修改配置文件’

---
+++ setup.sh
@@ -0,0 +1,20 @@
+# OSX
+# brew install gawk
+# brew install tmux
+# brew install tig
+# brew install vim --with-lua --with-override=system-vi --with-python
+# brew install thefuck
+# brew install bat # enhance cat command
+# brew install prettyping # enhance ping command
+# brew install fzf # replace Ctrl+R
+# brew install diff-so-fancy # work with git
+# brew install fd # enhance find command
+# brew install ncdu # enhance du -sh
+# brew install tldr # TL;DR replace man command
+
+# zsh plugins:
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# tmux plugins manager
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
