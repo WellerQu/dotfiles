8225dd75c16d065ce336202db205c444ab33f98e
nix <nix@mecx.com>
2024-08-17 13:24:09 +0800

    feat: setup

--- setup.sh
+++ setup.sh
@@ -18,3 +18,31 @@
 
 # tmux plugins manager
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+set -ex
+
+dotfiles=(".zshrc" ".vimrc" ".gitconfig" ".gitmessage" ".tmux.conf" ".myclirc")
+
+create_symlinks() {
+  for file in "${dotfiles[@]}"
+  do
+    ln -sf ${PWD}/${file} ${HOME}/${file}
+    echo link ${PWD}/${file} ${HOME}/${file}
+  done
+}
+
+remove_symlinks() {
+  for file in "${dotfiles[@]}"
+  do
+    unlink ${HOME}/${file}
+    echo unlink ${HOME}/${file}
+  done
+}
+
+if [ "$1" = "i" ]; then
+  create_symlinks
+fi
+
+if [ "$1" = "u" ]; then
+  remove_symlinks
+fi
